b5604ebb79cb89109952f3e44eed2534677fb8df
crispyyv <44894965+crispyyv@users.noreply.github.com>
2020-04-01 13:42:06 +0300

    Add files via upload

---
+++ precache-manifest.65198a0d148b83337e0dca09bbf31bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e381168ea4d53f93878b67a77e686c9",
+    "url": "/dots-game/index.html"
+  },
+  {
+    "revision": "5c84e370b01707c24a39",
+    "url": "/dots-game/static/css/main.b614f09f.chunk.css"
+  },
+  {
+    "revision": "be24200a3a342de0a360",
+    "url": "/dots-game/static/js/2.731349e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dots-game/static/js/2.731349e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c84e370b01707c24a39",
+    "url": "/dots-game/static/js/main.d41c07f1.chunk.js"
+  },
+  {
+    "revision": "4b519254952bcab595a1",
+    "url": "/dots-game/static/js/runtime-main.41faf950.js"
+  }
+]);
